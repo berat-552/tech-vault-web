164d7f962cdbeecf7eabba33536cd85618ce2bbb
berat-552 <berat.d7599@gmail.com>
2024-04-30 13:16:31 +0100

    fix theme toggler and add more theme options

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,55 +1,50 @@
 import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
+import { themes } from "../constants/themes";
 
 function ToggleThemeButton() {
-  const [theme, setTheme] = useState(Cookies.get("theme") || "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "default");
 
   useEffect(() => {
-    Cookies.set("theme", theme, { expires: 7 });
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
-  const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  const handleChange = (newTheme: string) => {
+    setTheme(newTheme);
   };
 
   return (
-    <label className="flex cursor-pointer gap-2 p-4">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+    <div className="dropdown mx-2 z-10">
+      <div tabIndex={0} role="button" className="btn m-1">
+        Theme
+        <svg
+          width="12px"
+          height="12px"
+          className="h-2 w-2 fill-current opacity-60 inline-block"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048"
+        >
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] shadow-2xl bg-base-300 rounded-box overflow-y-auto max-h-36"
       >
-        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-      </svg>
-      <input
-        type="checkbox"
-        value="light"
-        className="toggle theme-controller"
-        checked={theme === "light"}
-        onChange={toggleTheme}
-      />
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <circle cx="12" cy="12" r="5" />
-        <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-      </svg>
-    </label>
+        {themes.map((item) => (
+          <li key={item}>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label={item.charAt(0).toUpperCase() + item.slice(1)}
+              value={item}
+              checked={theme === item}
+              onChange={() => handleChange(item)}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
